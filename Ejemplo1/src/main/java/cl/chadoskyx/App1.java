03f086f72fad987ed77382f937b443be421c4538
Sebastián Salazar Molina <sebasalazar@gmail.com>
2015-04-13 18:18:18 -0300

    Refactor a archivo utilitario

--- Ejemplo1/src/main/java/cl/chadoskyx/App1.java
+++ Ejemplo1/src/main/java/cl/chadoskyx/App1.java
@@ -1,58 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cl.chadoskyx;
 
 import cl.chadoskyx.utils.ArchivoUtils;
-import static cl.chadoskyx.utils.ArchivoUtils.crearFichero;
 import cl.chadoskyx.utils.FechaUtils;
-import cl.chadoskyx.utils.NumeroUtils;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.Serializable;
 import java.util.Date;
 import java.util.Scanner;
-
-import org.apache.commons.lang3.StringUtils;
 
 /**
  *
  * @author Valery
  */
-public class App1 {
-    private static Object writer;
+public class App1 implements Serializable {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String sFichero = "Fechas Ingresadas.txt";
+
         int dia, mes, anio;
-        System.out.println("Introduzca su dia de nacimiento");
+        System.out.println("Introduzca su dia de nacimiento: ");
         dia = in.nextInt();
-        System.out.println("ingrese su mes");
+        System.out.println("ingrese su mes: ");
         mes = in.nextInt();
-        System.out.println("ingrese año");
+        System.out.println("ingrese año: ");
         anio = in.nextInt();
-        /*entrada = in.next();
-        // StringUtils.isNumeric(entrada == true)
-        if (NumeroUtils.esNumero(entrada)) {
-        System.out.println("Es un numero");
-        } else {
-        System.out.println("No es numero");
-        }*/
+
         Date crearFecha = FechaUtils.crearFecha(dia, mes, anio);
         String fechaEscrita = FechaUtils.fechaEscrita(crearFecha);
-        //String CrearFichero = crearFichero(sFichero);
-        //System.out.println(fechaEscrita);
-        BufferedWriter bw = new BufferedWriter (new FileWriter(sFichero, true));
-        bw.write(fechaEscrita);
-        bw.newLine();
-        bw.close();
-        
+        boolean ok = ArchivoUtils.agregarLineas(fechaEscrita);
+
+        System.out.printf("Fecha: '%s' # Resultado de escribirlo en archivo: '%s'", fechaEscrita, ok);
+        System.out.println();
     }
 }
--- Ejemplo1/src/main/java/cl/chadoskyx/utils/ArchivoUtils.java
+++ Ejemplo1/src/main/java/cl/chadoskyx/utils/ArchivoUtils.java
@@ -5,29 +5,56 @@
  */
 package cl.chadoskyx.utils;
 
+import java.io.BufferedWriter;
 import java.io.Serializable;
 import java.io.File;
-
+import java.io.FileWriter;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  *
  * @author Valery Soto Lastra <valery.soto17@gmail.com>
  */
 public class ArchivoUtils implements Serializable {
-    
-    private ArchivoUtils (){
+
+    // Definiremos un nombre que será siempre el mismo
+    private static final String NOMBRE_ARCHIVO = "fechas_ingresadas.txt";
+
+    private ArchivoUtils() {
         throw new AssertionError();
     }
-    
-    public static String crearFichero (String sFichero){
-        File fichero = new File (sFichero);
-        if (fichero.exists()){
-            System.out.println("El fichero existe");
+
+    /**
+     * Agrego líneas de texto a un archivo
+     *
+     * @param texto Texto a agregar al archivo
+     * @return true si se pudo agregar la línea, false en cualquier otro caso
+     */
+    public static boolean agregarLineas(String texto) {
+        boolean ok = false;
+        try {
+            // Si el texto que ingreso no está vacío lo agrego al archivo
+            if (StringUtils.isNotBlank(texto)) {
+                
+                String ruta = String.format("%s%s%s", 
+                        System.getProperty("user.dir"),
+                        System.getProperty("file.separator"),
+                        NOMBRE_ARCHIVO);
+                
+                // FileWriter requiere: la ruta del archivo y si se debe agregar la línea al archivo
+                BufferedWriter bw = new BufferedWriter(new FileWriter(ruta, true));
+                bw.write(texto);
+                bw.newLine();
+                bw.close();
+                
+                // Si no me he caido, quiere decir que pude escribir bien en el archivo
+                ok = true;
+            }
+        } catch (Exception e) {
+            // Si me caigo defino un error en el método.
+            ok = false;
+            // Más adelante vamos a loggear los errores.
         }
-        else{
-            System.out.println("El fichero no existe ");
-        }
-        return sFichero;
-    
+        return ok;
     }
 }
